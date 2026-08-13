6675ced4c7dcf71bbe4e07d7ec89e688d58f1e9f
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-13 16:40:10 -0300

    Remove conflicting cursor entry helper

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,8 +2,6 @@ import './application';
 import './dock-reveal';
 import './menu-bootstrap';
 import './menu-motion';
-import { setupPieceCursorEntryMotion } from './features/piece-cursor-entry-motion';
 import { setupScrollMarquee } from './features/scroll-marquee-boosted';
 
-setupPieceCursorEntryMotion();
 setupScrollMarquee();
