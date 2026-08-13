0fd53492d03da3efa3461715747e9e27f9b82080
krestosa <57479846+krestosa@users.noreply.github.com>
2026-08-13 16:03:11 -0300

    Use boosted marquee motion

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,6 +2,6 @@ import './application';
 import './dock-reveal';
 import './menu-bootstrap';
 import './menu-motion';
-import { setupScrollMarquee } from './features/scroll-marquee';
+import { setupScrollMarquee } from './features/scroll-marquee-boosted';
 
 setupScrollMarquee();
